a5a6dcf94755c4ab4d5ccf31e85b4a8afbcc8f50
UAA <uaa04@yandex.ru>
2023-03-16 19:11:14 +0900

    дз 4 семинар

--- HW_4/Main.java
+++ HW_4/Main.java
@@ -7,14 +7,13 @@ public class Main {
         MyList<Integer> list2 = new MyList<Integer>(new Integer[]{8, 10, 9, 0, -2, 200});
         MyList<Double> list3 = new MyList<Double>(new Double[]{8., 10., 9., 4., -10.});
 
-
         //System.out.println(list1.sumItems());
-        System.out.println(list2.exist(10));
+        //System.out.println(list2.exist(10));
         //System.out.println(list3.sumItems());
 
-        //list1.printList();
+        list1.printList();
         list2.printList();
-        //list3.printList();
+        list3.printList();
         
 
     }
